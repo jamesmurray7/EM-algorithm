2ca1e70435cb54b08b092647626e574a741ca72a
parqueted <j.murray7@ncl.ac.uk>
2021-02-22 14:14:22 +0000

    Adding -paramtol versions. Also updating to use faster matrix computation

--- fns/Classic-paramtol.R
+++ fns/Classic-paramtol.R
@@ -1,11 +1,12 @@
 #' ###############
-#' Defining functions for classical EM 
-#' This operates based on differences on the likelihood
+#' Updating first implementation of Classic EM
+#' to utilise maximum parameter difference rather than log-likelihood
+#' as this should save computation time.
 #' ###############
 
 tr <- function(x) sum(diag(x))
 ll <- function(X, Y, Z, beta, var.0, var.1){ 
-  V <- c(var.1) * tcrossprod(Z) + c(var.0) * diag(length(Y)) # tcrossprod
+  V <- c(var.1) * tcrossprod(Z) + c(var.0) * diag(length(Y)) # sigma.u * ZZt + sigma.eIn
   V.sqrt <- chol(V)
   detV <- sum(log(diag(V.sqrt))) * 2
   # X, beta and residuals
@@ -13,13 +14,13 @@ ll <- function(X, Y, Z, beta, var.0, var.1){
   resid <- Y - Xb
   temp <- sum(forwardsolve(l = t(V.sqrt), x = resid) ^ 2)
   return(
-    -0.5*length(Y)*log(2*pi) + -0.5 * temp - 0.5 * detV
+    -0.5*length(Y)*log(2*pi) -0.5 * temp - 0.5 * detV
   )
 }
 
 # Define EM function ------------------------------------------------------
 em <- function(X,Y,Z, init.beta = beta0, init.var.0 = var.0, init.var.1 = var.1,
-                       tol = 1e-5, maxiter = 2000, history = F){
+                       tol = 1e-3, maxiter = 2000, history = F){
   message("EM Algorithm ----\nDimensions: ", nrow(Y), " x ", ncol(X)-1, 
           "\nInitial estimates: beta =  ", init.beta, "\nsigma0 = ", sqrt(init.var.0),"; sigma1 = ", sqrt(init.var.1),
           "tol = ", tol, " maximum iterations = ", maxiter)
@@ -33,15 +34,14 @@ em <- function(X,Y,Z, init.beta = beta0, init.var.0 = var.0, init.var.1 = var.1,
     beta <- matrix(beta, ncol = 1)
   }
   
-  # Store iteration history if wanted.
-  if(history) iter.hist <- data.frame(iter, t(beta), var.0=sqrt(var.0), var.1=sqrt(var.1), 
-                                      ll(X,Y,Z,beta,var.0,var.1))
-  
+  params <- c(t(beta), var.0, var.1)
+  if(history) iter.hist <- data.frame(iter, t(params))
+
   n <- length(Y); q1 <- ncol(Z)
   
   t0 <- proc.time()[3]
+  # Begin while loop
   while(diff > tol & iter <= maxiter){
-    L0 <- ll(X, Y, Z,  beta, var.0, var.1) 
     
     # E step ----
     # Covariance stuff
@@ -49,10 +49,9 @@ em <- function(X,Y,Z, init.beta = beta0, init.var.0 = var.0, init.var.1 = var.1,
     Vinv <- solve(V)
     V.sqrt <- chol(V)
     # X %*% beta and resulting residuals
-    Xtx <- crossprod(X)
+    XtX <- crossprod(X)
     Xb <- X %*% beta
     resid <- Y-Xb
-    
     # Calculate expectations uiuiT (call this u.0 (ie eps) and u.1)
     z <- forwardsolve(t(V.sqrt), Z)
     u.0 <- c(var.0)^2 * crossprod(resid, Vinv) %*% Vinv %*% resid +
@@ -67,30 +66,35 @@ em <- function(X,Y,Z, init.beta = beta0, init.var.0 = var.0, init.var.1 = var.1,
     var.1.new <- u.1/q1
     beta.new <- solve(XtX) %*% t(X) %*% w
     
-    # New ll
-    L1 <- ll(X, Y, Z, beta.new, var.0.new, var.1.new)
-    diff <- abs(L1-L0)
-    # Print out
-    L0.round <- round(L0, 5); L1.round <- round(L1,5); diff.round <- round(diff, 11)
+    # New parameter set
+    params.new <- c(t(beta.new), var.0.new, var.1.new)
+    diffs <- abs(params.new-params)
+    diff <- max(diffs)
+    print(params)
+    print(params.new) 
+    print(diffs)
+    print(diff)
     
-    message("Iteration ", iter, " diff = ", diff.round)
+    message("Iteration ", iter, " largest difference = ", diff)
+    
     # Set new parameters
     var.0 <- var.0.new
     var.1 <- var.1.new
     beta <- beta.new
     iter <- iter + 1
+    params <- c(t(beta), var.0, var.1) # 60 minutes before realised this was missing !
     
-    if(history) iter.hist <- rbind(iter.hist, c(iter, t(beta), sqrt(var.0), sqrt(var.1), l1))
+    # Record history if needed
+    if(history) iter.hist <- rbind(iter.hist, c(iter, t(params.new)))
   }
   t1 <- proc.time()[3]
-  message("EM converged after ", iter - 1, " iterations, this took ", round(t1-t0, 2), " seconds.")
-  
+  message("EM Complete after ", iter, " iterations, this took ", round(t1-t0,2), " seconds.")
   if(history){
     rtn <- list(beta = beta, sigma.0 = sqrt(var.0), sigma.1 = sqrt(var.1), iter.hist = iter.hist,
-                time = t1-t0, iter = iter)
+                time = t1-t0, iter = iter, ll = ll(X,Y,Z,beta,var.0,var.1))
   }else{
-    rtn <- list(beta = beta, sigma.0 = sqrt(var.0), sigma.1 = sqrt(var.1), time = t1-t0, iter = iter)
+    rtn <- list(beta = beta, sigma.0 = sqrt(var.0), sigma.1 = sqrt(var.1),
+                time = t1-t0, iter = iter, ll = ll(X,Y,Z,beta,var.0,var.1))
   }
-  
-  return(rtn)
+  rtn
 }
