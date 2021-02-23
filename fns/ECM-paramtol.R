d53a93b574dae8ffbdfc0a25bc9ba55adfdf2f3a
parqueted <j.murray7@ncl.ac.uk>
2021-02-23 12:34:31 +0000

    Initial commit of ECM on parameter tolerance

--- fns/ECM-paramtol.R
+++ fns/ECM-paramtol.R
@@ -1,6 +1,5 @@
 #' ###############
-#' Defining functions for Expectation Conditional Maximisation
-#' with and without iteration history
+#' ECM Algorithm using stopping criteria based on 
 #' Based on poc-scripts/ECM and fns/Classical
 #' ###############
 
@@ -11,7 +10,7 @@ rm(list = ls())
 tr <- function(x) sum(diag(x))
 # Log likelihood
 ll <- function(X, Y, Z, beta, var.0, var.1){ 
-  V <- c(var.1) * Z %*% t(Z) + c(var.0) * diag(length(Y))
+  V <- c(var.1) * tcrossprod(Z) + c(var.0) * diag(length(Y))
   V.sqrt <- chol(V)
   detV <- sum(log(diag(V.sqrt))) * 2
   # X, beta and residuals
@@ -19,17 +18,16 @@ ll <- function(X, Y, Z, beta, var.0, var.1){
   resid <- Y - Xb
   temp <- sum(forwardsolve(l = t(V.sqrt), x = resid) ^ 2)
   return(
-    -0.5 * temp - 0.5 * detV
+    -0.5*length(Y)*log(2*pi) + -0.5 * temp - 0.5 * detV
   )
 }
 # Computation of covariance matrix
-getV <- function(var.0,var.1,n) c(var.1) * Z %*% t(Z) + c(var.0) * diag(n)
+getV <- function(var.0,var.1,n) c(var.1) * tcrossprod(Z) + c(var.0) * diag(n)
 
 
 # ECM function 
-
 ecm <- function(X, Y, Z, init.beta, init.var.0, init.var.1,
-                tol = 1e-10, maxiter = 2e3, history = F){
+                tol = 1e-3, maxiter = 2e3, history = F){
   n <- length(Y)
   q1 <- ncol(Z)
   diff <- 10
@@ -40,19 +38,21 @@ ecm <- function(X, Y, Z, init.beta, init.var.0, init.var.1,
     beta <- matrix(beta, ncol = 1)
   }
 
-  if(history) iter.hist <- data.frame(iter, t(beta), var.0=sqrt(var.0), var.1=sqrt(var.1), 
-                                      ll(X,Y,Z,beta,var.0,var.1))
+  # First parameter vector
+  params <- c(t(beta), var.0, var.1)
+  
+  # Covariance matrix for iter 0
+  V <- getV(var.0, var.1, n)
+  Vinv <- solve(V)
+  V.sqrt <- chol(V)
+  
+  if(history) iter.hist <- data.frame(iter, t(beta), var.0=sqrt(var.0), var.1=sqrt(var.1))
   
   t0 <- proc.time()[3]
   while(diff > tol & iter <= maxiter){
-    l0 <- ll(X,Y,Z, beta, var.0, var.1)
     
     # E step ----
-    # Covariance stuff
     
-    V <- getV(var.0, var.1, n)
-    Vinv <- solve(V)
-    V.sqrt <- chol(V)
     # X %*% beta and resulting residuals
     XtX <- crossprod(X)
     Xb <- X %*% beta
@@ -60,37 +60,43 @@ ecm <- function(X, Y, Z, init.beta, init.var.0, init.var.1,
     
     # Calculate expectations uiuiT (call this u.0 (ie eps) and u.1)
     z <- forwardsolve(t(V.sqrt), Z)
-    u.0 <- c(var.0)^2 * t(resid) %*% Vinv %*% Vinv %*% resid + 
+    u.0 <- c(var.0)^2 * crossprod(resid, Vinv) %*% Vinv %*% resid + 
       tr(c(var.0) * diag(n) - c(var.0)^2 * Vinv)
-    u.1 <- c(var.1)^2 * t(resid) %*% Vinv %*% Z %*% t(Z) %*% Vinv %*% resid + 
+    u.1 <- c(var.1)^2 * crossprod(resid, Vinv) %*% tcrossprod(Z) %*% Vinv %*% resid + 
       tr(c(var.1) * diag(q1) - c(var.1)^2 * crossprod(z))
     
     # M-step ----
+    
     # CM step 1 ---
     # Estimate \sigma^(t+1)
     var.0.new <- u.0/n
     var.1.new <- u.1/q1
     message("CM step 1 done, new sigma = [", sqrt(var.0.new), ", ", sqrt(var.1.new),"]")
+    
     # CM step 2 ---
     V <- getV(var.0.new, var.1.new, n)
     Vinv <- solve(V)
-    Gamma <- X %*% beta + c(var.0.new) * Vinv %*% resid
+    V.sqrt <- chol(V)
+    Gamma <- Xb + c(var.0.new) * Vinv %*% resid
     # And estimate \beta
-    beta.new <- solve(XtX) %*% t(X) %*% Gamma
+    beta.new <- solve(XtX) %*% crossprod(X, Gamma)
     message("CM step 2 done, new beta = ", beta.new)
     
-    l1 <- ll(X,Y,Z, beta.new, var.0.new, var.1.new)
     
-    # New ll
-    diff <- abs(l1-l0)
-    # Print out
-    L0.round <- round(l0, 5); L1.round <- round(l1,5); diff.round <- round(diff, 11)
-    message("Iteration ", iter, " diff = ", diff.round)
+    
+    # New parameter vector, and differences
+    params.new <- c(t(beta.new), var.0.new, var.1.new)
+    diffs <- abs(params.new-params)
+    diff <- max(diffs)
+    message("iteration ", iter + 1, " largest difference = ", diff)
+    
     # Set new parameters
     var.0 <- var.0.new
     var.1 <- var.1.new
     beta <- beta.new
+    params <- c(t(beta), var.0, var.1)
     iter <- iter + 1
+    
     
     # Record history if required
     if(history) iter.hist <- rbind(iter.hist, c(iter, t(beta), sqrt(var.0), sqrt(var.1), l1))
