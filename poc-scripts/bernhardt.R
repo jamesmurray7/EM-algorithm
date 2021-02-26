fd5f6496c000d6d6f12a2a868c656fedff4e317a
parqueted <j.murray7@ncl.ac.uk>
2021-02-26 15:40:08 +0000

    Trying different things

--- poc-scripts/bernhardt.R
+++ poc-scripts/bernhardt.R
@@ -14,10 +14,10 @@ x <- simlong(num_subj = 100, num_times = 5)
 X <- x$X; Y <- x$Y; Z <- x$Z
 beta0 <- x$lmer.fit@beta
 var.0.init <- as.matrix(sigma(x$lmer.fit)^2)
-var.1.init <- as.matrix(as.numeric(summary(x$lmer.fit)$varcor[1]))
+var.1.init <- as.matrix(as.numeric(summary(x$lmer.fit)$varcor[1])) # = D in univariate case.
 theta0 <- c(t(beta0), var.0.init, var.1.init)
 # Initial estimates for bi
-b.inits <- x$lmer.fit@u
+b.inits <- ranef(x$lmer.fit)$id$`(Intercept)`
 # Estimate for B, 
 B <- mean(b.inits)
 
@@ -48,6 +48,21 @@ b.dens.univ <- function(b, b.input){
 
 nlminb(c(0,1), b.dens.univ, NULL, NULL, b.inits[1], lower = c(-4,0.01), upper = c(4,2))
 # Same results as initial estimates so doing something wrong here!
+
+# Is it supposed to be log-likelihood?
+b.ll <- function(b, b.input){
+  # b[1] = mu; b[2] = sigma^2
+  ll <- -1/2 * log(2 * pi) - 1/2 * log(b[2]) - 1/(2*b[2]) * (b.input-b[1])^2
+}
+
+b.hat <- c()
+sigma.hat <- c()
+for(i in 1:length(b.inits)){
+  op <- optim(c(0,0.5), b.ll, NULL, b.inits2[i],
+              control = list(fnscale = -1), method = "L-",
+              lower = c(min(b.inits), 0.01), upper = c(max(b.inits), 2))
+  b.hat[i] <- op$par[1]; sigma.hat[i] <- op$par[2]
+}
 
 # Update parameter vector \bm{\theta} via EM steps ------------------------
 B.new <- mean(b.hat)
